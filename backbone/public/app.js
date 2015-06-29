1d2f62aeb1fc558f322e22ec71fd06bc02259d56
Michael Tran <koramaiku@Michaels-MacBook-Pro-2.local>
2015-06-29 18:07:19 +1000

    Finsished 2.6 and the Backbone Implmementation

--- backbone/public/app.js
+++ backbone/public/app.js
@@ -21,6 +21,7 @@ var ContactsListView = Backbone.View.extend({
     }
 });
 
+
 var ContactsItemView = Backbone.View.extend({
     tagName: 'li',
     events: {
@@ -38,12 +39,65 @@ var ContactsItemView = Backbone.View.extend({
     }
 });
 
+var NewContactView = Backbone.View.extend({
+    template: $('#NewContactViewTemplate').html(),
+    render: function(){
+        this.el.innerHTML = this.template;
+        return this;
+    },
+    events: {
+        'click button': 'add'
+    },
+    add: function(){
+        this.collection.create({
+            firstName: this.$('.firstName').val(),
+            lastName: this.$('.lastName').val(),
+            email: this.$('.email').val()
+        }, {
+            success: function(){
+                Backbone.history.navigate('', { trigger: true });
+            }
+        });
+    }
+
+});
+
 var ActionsView = Backbone.View.extend({
     className: 'actions',
     template: '<a href="/contacts/new">New Contact</a>',
     render: function(){
         this.el.innerHTML = this.template;
         return this;
+    }
+});
+
+
+var ContactView = Backbone.View.extend({
+    template: _.template($('#ContactViewTemplate').html()),
+    initialize: function(){
+        this.listenTo(this.model, 'change:firstName change:lastName', this.updateName);
+    },
+    events: {
+        'click .update': 'update',
+        'click .delete': 'delete'
+    },
+    render: function(){
+        this.el.innerHTML = this.template(this.model.toJSON());
+        return this;
+    },
+    update: function(){
+        this.model.save({
+            firstName: this.$('.firstName').val(),
+            lastName: this.$('.lastName').val(),
+            email: this.$('.email').val()
+        });
+    },
+    updateName: function(){
+        this.$('h2').text(this.model.get('firstName') + ' ' + this.model.get('lastName'));
+    },
+    delete: function(){
+        this.model.destroy();
+        Backbone.history.navigate('', {trigger: true});
     }
 });
 
@@ -68,10 +122,12 @@ var Router = Backbone.Router.extend({
             .append(new ContactsListView({ collection: this.contacts }).render().el);
     },
     displayForm: function(){
-        console.log('form');
+        this.el.empty()
+            .append(new NewContactView({ collection: this.contacts }).render().el)
     },
     displaySingle: function(id){
-        console.log(this.contacts.get(parseInt(id)).toJSON());
+        var model = this.contacts.get(parseInt(id, 10));
+        this.el.empty().append(new ContactView({model: model}).render().el);
     }
 });
 
